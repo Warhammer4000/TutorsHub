c91f6ff4c6f25c6c316d32575fc044d62cacd717
Tazim <tazimtazim2012@hotmail.com>
2017-10-10 05:25:47 +0600

    Add project files.

---
+++ ATP2.Profile/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ATP2.Profile.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Tanimul Haque Khan";
+            ViewBag.Email = "tazimtaizm2012@gmail.com";
+         
+            ViewBag.Gender0 = true;
+            ViewBag.Gender1 = false;
+            ViewBag.Gender2 = false;
+            ViewBag.Day = 29;
+            ViewBag.Month = 4;
+            ViewBag.Year = 1995;
+            ViewBag.Degree0 = true;
+            ViewBag.Degree1 = true;
+            ViewBag.Degree2 = true;
+            ViewBag.Degree3 = false;
+            return View(ViewBag);
+        }
+    }
+}
---
+++ ATP2.Profile/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ATP2.Profile.MvcApplication" Language="C#" %>
