6672b04f8d1f56195f06faceacee845327112f7e
Tazim <tazimtazim2012@hotmail.com>
2017-11-01 09:10:18 +0600

    exam controller added

---
+++ ATP2.Profile/Controllers/ExamController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ATP2.Profile.Controllers
+{
+    public class ExamController : Controller
+    {
+       [HttpGet]
+        public ActionResult Result()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public ActionResult Exam()
+        {
+            return View();
+        }
+
+
+    }
+}
