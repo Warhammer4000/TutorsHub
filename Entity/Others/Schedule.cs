ed0289e7d1f9880f72cf3b61d26a640a557d60f9
Tazim <tazimtazim2012@hotmail.com>
2017-12-31 12:02:59 +0600

    Schedule recieved properly as JSON string Some html reworks needed.

--- Entity/Others/Schedule.cs
+++ Entity/Others/Schedule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Entity.Others
 {
@@ -10,24 +11,34 @@ namespace Entity.Others
     {
         public Dictionary<string,Timings> WeekDays { get; set; }
 
+        public string ScheduleAsString {
+            get => JsonConvert.SerializeObject(WeekDays);
+            set => WeekDays = JsonConvert.DeserializeObject<Dictionary<string, Timings>>(value);
+        }
         public Schedule()
         {
-            WeekDays.Add("Saturday",new Timings());
-            WeekDays.Add("Sunday", new Timings());
-            WeekDays.Add("Monday", new Timings());
-            WeekDays.Add("Tuesday", new Timings());
-            WeekDays.Add("Wednesday", new Timings());
-            WeekDays.Add("Thursday", new Timings());
-            WeekDays.Add("Friday", new Timings());
+            WeekDays = new Dictionary<string, Timings>
+            {
+                {"Saturday", new Timings()},
+                {"Sunday", new Timings()},
+                {"Monday", new Timings()},
+                {"Tuesday", new Timings()},
+                {"Wednesday", new Timings()},
+                {"Thursday", new Timings()},
+                {"Friday", new Timings()}
+            };
         }
+
+
+
     }
 
     public class Timings
     {
-        public Dictionary<string, string> Times { get; set; }
-        //Keys will have time as string and values would be user's key[For now Email]
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public string Meridium { get; set; }
 
-        public string tim { get; set; } 
         
 
     }
